2563f0b7c976a360f44531efa78e907915e49a7a
Jie,Nissel <jie.nissel@lowes.com>
2022-03-25 13:09:59 -0400

    have notebook to better visual

--- 2022-03-25.py
+++
@@ -1,9 +0,0 @@
-
-# %% [markdown] 
-# [Shared by Teammate: Add this single word to make your Pandas Apply faster; Easy to implement Parallelization
-#* Add this single word to make your Pandas Apply faster | by Rahul Agarwal | Towards Data Sciencehttps://towardsdatascience.com/add-this-single-word-to-make-your-pandas-apply-faster-90ee2fffe9e8
-#]
-
-# %% 
-import pandas as pd
-import numpy as np
